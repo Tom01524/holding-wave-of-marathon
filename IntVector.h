bd281e21ccfd2ca91df8c5d04f9657ea0a8ab0fc
Tom01524 <128370272+Tom01524@users.noreply.github.com>
2024-10-02 22:31:20 +0800

    Update IntVector.h
    create new not to use (#define)

--- IntVector.h
+++ IntVector.h
@@ -1,6 +1,6 @@
 
 void init();
-void arrMove();
+//void arrMove();
 void expand(int _mul) ;
 void print();
 void add(int var1);
@@ -10,12 +10,12 @@ void freeArr();
 
 struct _IntVec{
     int* arr;
-    int* nextArr;
+    //int* nextArr;
     int max;
     int usedBefore;
 
     void (*init)();
-    void (*arrMove)();
+    //void (*arrMove)();
     void (*expand)(int _mul) ;
     void (*print)();
     void (*add)(int var1);
@@ -26,10 +26,10 @@ struct _IntVec{
 
 };
 
-struct _IntVec VectorInt = {
-        .max = -1,
+struct _IntVec VectorInt= { // 初始化结构体 max = -1
+        .max = -1,          // 函数指针 对应函数首地址
         .init = &init,
-        .arrMove = &arrMove,
+        //.arrMove = &arrMove,
         .expand = &expand,
         .print = &print,
         .add = &add,
@@ -37,61 +37,62 @@ struct _IntVec VectorInt = {
         .size = &size,
         .freeArr = &freeArr
 };
+struct _IntVec* VecIntPtr = &VectorInt;
 
 void init(){
     fputs("~~first init\n",stdout);
-    VectorInt.arr = (int*)malloc(10*sizeof(int));
-    VectorInt.max = 10;
-    VectorInt.usedBefore = 0;
+    VecIntPtr->arr = (int*)malloc(10*sizeof(int));
+    VecIntPtr->max = 10;
+    VecIntPtr->usedBefore = 0;
 }
-void arrMove(){
-    for(int i=0;i<VectorInt.usedBefore;i++){
-        VectorInt.nextArr[i] = VectorInt.arr[i];
-        //printf("moving: %d\n", arr[i]);
-    }
-}
+//void arrMove(){
+//    for(int i=0;i<VecIntPtr->usedBefore;i++){
+//        VecIntPtr->nextArr[i] = VecIntPtr->arr[i];
+//        //printf("moving: %d\n", arr[i]);
+//    }
+//}
 void expand(int _mul) {
-
-    while((VectorInt.nextArr = (int*)malloc(VectorInt.max * _mul * sizeof(int))) == NULL); // 扩容
+    // 扩容 不为NULL
+    while((VecIntPtr->arr = (int*)realloc(VecIntPtr->arr,VecIntPtr->max * _mul * sizeof(int))) == NULL){}
 
     fputs("~~realloc\n",stdout);
-    arrMove(); // 扩容
-    fputs("~~arr move\n",stdout);
-    free(VectorInt.arr);
+    //arrMove(); // 扩容
 
-    VectorInt.arr = VectorInt.nextArr; // 扩容
-    VectorInt.max *= _mul;
-    printf("~~max: %d\n",VectorInt.max);
+    //free(VecIntPtr->arr);
+
+    //VecIntPtr->arr = VecIntPtr->nextArr; // 扩容
+    VecIntPtr->max *= _mul;
+    printf("~~max: %d\n",VecIntPtr->max);
 }
 void print() {
-    for (int i = 0; i < VectorInt.usedBefore; i++) {
-        if (i == 0)printf("[%d, ", VectorInt.arr[i]);
-        else if (i == VectorInt.usedBefore - 1)printf("%d]\n", VectorInt.arr[i]);
-        else printf("%d, ", VectorInt.arr[i]);
+    for (int i = 0; i < VecIntPtr->usedBefore; i++) {
+        if (i == 0)printf("[%d, ", VecIntPtr->arr[i]);
+        else if (i == VecIntPtr->usedBefore - 1)printf("%d]\n", VecIntPtr->arr[i]);
+        else printf("%d, ", VecIntPtr->arr[i]);
     }
 }
 void add(int var1){
-    if(VectorInt.max == -1){VectorInt.init();} // 初始化
-    if(VectorInt.usedBefore<VectorInt.max){
-        VectorInt.arr[VectorInt.usedBefore] = var1;
-        VectorInt.usedBefore++;
+    if(VecIntPtr->max == -1){VecIntPtr->init();} // 初始化
+    if(VecIntPtr->usedBefore<VecIntPtr->max){
+        VecIntPtr->arr[VecIntPtr->usedBefore] = var1;
+        VecIntPtr->usedBefore++;
     }else{
-        VectorInt.expand(2);
+        VecIntPtr->expand(2);
 
-        VectorInt.arr[VectorInt.usedBefore] = var1;
-        VectorInt.usedBefore++;
+        VecIntPtr->arr[VecIntPtr->usedBefore] = var1;
+        VecIntPtr->usedBefore++;
     }
 }
 int get(int index1){
-    return VectorInt.arr[index1];
+    return VecIntPtr->arr[index1];
 }
 int size(){
-    return VectorInt.usedBefore;
+    return VecIntPtr->usedBefore;
 }
 void freeArr(){
-    free(VectorInt.arr);
-    VectorInt.max = -1;
-    VectorInt.usedBefore = 0;
-    VectorInt.arr = NULL;
-    VectorInt.nextArr = NULL;
+    free(VecIntPtr->arr);
+    VecIntPtr->max = -1;
+    VecIntPtr->usedBefore = 0;
+    VecIntPtr->arr = NULL;
+    //VecIntPtr->nextArr = NULL;
 }
